e0c5cd0a2c97bc3b1a6951a89bdb761af0f5e4e7
madheus <mat.leiria@gmail.com>
2023-10-24 18:43:22 -0300

    24

--- lista/quinto.js
+++ lista/quinto.js
@@ -1,10 +1,15 @@
 const rl = require('readline-sync')
 let dia = rl.question('que dia é hoje: ')
+while(true) {
 if (dia === 'sabado') {
     console.log('final de semana')
+    break
 }
 if (dia === 'domingo') {
     console.log('final de semana')
+    break
 } else {
     console.log('dia util')
+    break
+}
 }
