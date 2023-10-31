02355f3a821944fdb7aa0ac896c04005fdd6b1fc
madheus <mat.leiria@gmail.com>
2023-10-30 21:36:51 -0300

    30-10

--- projeto/chefinal.js
+++ projeto/chefinal.js
@@ -30,6 +30,10 @@ class Guerreiro extends Personagem {
         this.saude += cura;
         console.log(`Guerreiro se curou e recuperou ${cura} de saúde!`);
     }
+    inventario(){
+        let mochila = []
+        console.log('')
+    }
 }
 
 class Monstro extends Personagem {
@@ -54,6 +58,8 @@ while (true) {
     console.log(`opcoes do que fazer;
         explorar;
         taberna;
+        procurar itens:
+        inventario
         sair do jogo(digite: sair);
 
     `)
@@ -97,7 +103,8 @@ while (true) {
         console.log(`o que fazer:
         beber;
         hospedagem;
-        sair; `)
+        sair;
+         `)
         let escolha2 = rl.question('voce deseja fazer:  ')
 
         if (escolha2 == 'beber') {
@@ -113,5 +120,36 @@ while (true) {
     if (escolha1 == 'sair') {
         console.log('obrigado por jogar')
         break
+    }
+    if(escolha1=='procurar'){
+        let sorte= (Math.random() * 20)
+        if(sorte>11){
+                console.log(`voce achou um item
+                detalhe: este item é aleatorio ent tera que ver seu inventario`)
+            if(sorte==12){
+                mochila.push('Espada comum, seu valor de venda é de 10 moedas')
+            }if(sorte==13){
+                mochila.push('Escudo comum, seu valor de venda é de 12 moedas')
+            }if(sorte==14){
+                mochila.push('Espada incomum, seu valor de venda é de 15 moedas')
+            }if(sorte==15){
+                mochila.push('Escudo incomum, seu valor de venda é de 17 moedas')
+            }if(sorte==16){
+                mochila.push('Espada rara, seu valor de venda é de 25 moedas')
+            }if(sorte==17){
+                mochila.push('Escudo raro, seu valor de venda é de 27 moedas')
+            }if(sorte==18){
+                mochila.push('Espada epica, seu valor de venda é de 50 moedas')
+            }if(sorte==19){
+                mochila.push('Escudo epico, seu valor de venda é de 60 moedas')
+            }
+        }
+        if(sorte<=10){
+            console.log('voce se empenhou na procura, porem não achou nada, tente uma proxima vez')
+        }
+
+    }
+    if(escolha1=='inventario'){
+        Array.mochila.letgh
     }
 }
