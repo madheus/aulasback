d6bfd4a9c8dcb9ae36fd0d8a3f14f4a4a3cd270b
madheus <mat.leiria@gmail.com>
2023-11-01 21:42:45 -0300

    1-11

--- app.orcamentopessoal/app.js
+++ app.orcamentopessoal/app.js
@@ -1,1 +1,20 @@
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor){
+        this.ano=ano 
+        this.mes=mes 
+        this.dia=dia 
+        this.tipo=tipo 
+        this.descricao=descricao 
+        this.valor=valor
+    }
+    validarDados(){
+        for(let i in this) {
+            if(this[i] == undefined || this[i] == "" || this[i] == null){
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+}
 
